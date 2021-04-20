9eca4c8780b1a56277d6338886ecabadb00cb2ec
Niamh Idehen <niamh.idehen2@mail.dcu.ie>
2021-04-20 15:18:04 +0000

    Configured Django Template

--- book_root/book_site/settings.py
+++ book_root/book_site/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -56,10 +57,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'book_site.urls'
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [STATIC_ROOT, os.path.join(BASE_DIR, 'book_site/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,4 +125,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_SUFFIX = '/static/'
+STATIC_URL = FORCE_SCRIPT_NAME + STATIC_SUFFIX
--- book_root/pages/views.py
+++ book_root/pages/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Page
 
-def index(request):
-    return HttpResponse("<h1> Heyaaaaa </h1>")
+def index(request, pagename = ''):
+    pagename = '/' + pagename
+    pg = Page.objects.get(permalink=pagename)
+    context = {
+	'title':pg.title,
+	'content':pg.bodytext,
+    }
+    return render(request, 'base.html', context)
 
 # Create your views here.
